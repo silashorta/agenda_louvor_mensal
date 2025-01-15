45592f0e805f04da7e350099cda7a0897c5fbe83
silashorta <92862835+silashorta@users.noreply.github.com>
2025-01-14 23:08:49 -0300

    .

--- db/routes/index.js
+++
@@ -1,9 +0,0 @@
-const bodyParser = require("body-parser");
-const integrantes = require("./rotaAgenda");
-
-module.exports = (app) => {
-  app.use(bodyParser.json());
-  app.use(integrantes);
-
-  app.get('/', (req, res) => res.send('Ola servidor rodando'));
-};
